695168a849c166bd191db6909af532fbb99e5ec1
Mitchell Roe <mitchell.roe@erasys.de>
2025-06-05 09:24:36 +0200

    added a fibonacci sequence calculator

---
+++ ch03/fibonacci/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+fn main() {
+    println!("How many sequences into Fibonacci do you want to go?");
+    let dest_num = get_dest_num();
+    let final_num = calc_fib(dest_num);
+    println!("The {dest_num} number in the Fibonacci sequence is {final_num}");
+}
+
+fn get_dest_num() -> u32 {
+    loop {
+        let mut dest_num = String::new();
+        io::stdin()
+            .read_line(&mut dest_num)
+            .expect("Failed to read line");
+        match dest_num.trim().parse() {
+            Ok(num) => { break num; },
+            Err(_) => {
+                println!("Invalid number, please try again");
+                continue;
+            },
+        }
+    }
+}
+
+fn calc_fib(target: u32) -> u32 {
+    match target {
+        0 => 0,
+        1 => 1,
+        2 => 1,
+        _ => {
+            let mut prev = 1;
+            let mut sum = 2;
+            for _ in 2..target {
+                let old_sum = sum;
+                sum = sum + prev;
+                prev = old_sum;
+            }
+            sum
+        },
+    }
+}
