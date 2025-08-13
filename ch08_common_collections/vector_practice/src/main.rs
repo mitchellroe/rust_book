b60c027fce17eeb295d30fd31dc24df4e53c6cf1
Mitchell Roe <mitchell.roe@erasys.de>
2025-08-13 12:47:14 +0200

    moved things into individual scopes so I can keep using the same file

--- ch08_common_collections/vector_practice/src/main.rs
+++ ch08_common_collections/vector_practice/src/main.rs
@@ -1,41 +1,61 @@
 fn main() {
+    println!("# Creating a new vector"); // {{{
     {
+        // Option 1
         let _v: Vec<i32> = Vec::new();
+        // Option 2
         let _v = vec![1, 2, 3];
-    }
+    } // }}}
 
+    println!("# Updating a Vector"); // {{{
     {
+        // In order to append values, it needs to be mutable.
         let mut v = Vec::new();
         v.push(5);
         v.push(6);
         v.push(7);
         v.push(8);
-    }
+    } // }}}
 
+    println!("# Reading Elements of Vectors"); // {{{
     {
-        let v = vec![1, 2, 3, 4, 5];
-        let v_third: &i32 = &v[2];
-        println!("The third element is {v_third}");
+        {
+            let v = vec![1, 2, 3, 4, 5];
+            let third: &i32 = &v[2];
+            println!("The third element is {third}");
 
-        let v_third: Option<&i32> = v.get(2);
-        match v_third {
-            Some(third) => println!("The third element is {third}"),
-            None => println!("There is no third element"),
+            let third: Option<&i32> = v.get(2);
+            match third {
+                Some(third) => println!("The third element is {third}"),
+                None => println!("There is no third element"),
+            }
         }
-    }
 
-    let v5 = vec![1, 2, 3, 4, 5];
-    // let _does_not_exist = &v5[100];
-    let _does_not_exist = v5.get(100);
-    dbg!(_does_not_exist);
+        {
+            let v = vec![1, 2, 3, 4, 5];
+            // // The following will fail
+            // let does_not_exist = &v[100];
+            let does_not_exist = v.get(100);
+            dbg!(does_not_exist);
+        }
 
-    let v6 = vec![100, 32, 57];
-    for i in &v6 {
-        println!("{i}");
-    }
+        // {
+        // // The following will fail to compile because we have a mutable reference to a vector,
+        // // followed by an immutable reference. We cannot have both.
+        // let mut v = vec![1, 2, 3, 4, 5];
+        // let first = &v[0];
+        // v.push(6);
+        // println!("The first element is {first}");
+        // }
+    } // }}}
 
-    let mut v7 = vec![100, 32, 57];
-    for i in &mut v7 {
-        *i += 50;
-    }
+    println!("# Iterating Over the Values in a Vector"); // {{{
+    {
+        let v = vec![100, 32, 57];
+        for i in &v {
+            println!("{i}");
+        }
+    } // }}}
 }
+
+// vim:foldmethod=marker:
