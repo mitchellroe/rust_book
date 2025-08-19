f6a44052d19bc3db0e80e5fdeb0f005bbe590452
Mitchell Roe <mitchell.roe@erasys.de>
2025-08-19 17:23:55 +0200

    started the last section of chapter 8

--- ch08_common_collections/vector_practice/src/main.rs
+++ ch08_common_collections/vector_practice/src/main.rs
@@ -3,8 +3,9 @@
 
 fn main() {
     println!("# Chapter 8: Common Collections");
-    ch8_1();
-    ch8_2();
+    // ch8_1();
+    // ch8_2();
+    ch8_3();
 }
 
 fn ch8_1() {
@@ -251,6 +252,34 @@ fn ch8_2() {
     // }}}
 
     println!("### Strings Are Not So Simple"); // {{{
+    // }}}
+
+    // }}}
+}
+
+fn ch8_3() {
+    println!("## 8.3 Storing Keys with Associated Values in Hash Maps"); // {{{
+
+    println!("### Creating a New Hash Map"); // {{{
+    {
+        use std::collections::HashMap;
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+    }
+    // }}}
+
+    println!("### Accessing Values in a Hash Map"); // {{{
+    {
+        use std::collections::HashMap;
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+        let team_name = String::from("Blue");
+        let score = scores.get(&team_name).copied().unwrap_or(0);
+        dbg!(team_name);
+        dbg!(score);
+    }
     // }}}
 
     // }}}
