6edc63cde78520b08f374a29adc32bdd1c939576
Mitchell Roe <mitchell.roe@erasys.de>
2025-08-20 10:24:43 +0200

    worked on 8.3

--- ch08_03_storing_keys_with_associated_values_in_hash_maps/src/main.rs
+++ ch08_03_storing_keys_with_associated_values_in_hash_maps/src/main.rs
@@ -1,33 +1,49 @@
 #[allow(unused)]
 
 fn main() {
-    ch8_3();
-}
+    println!("# 8.3 Storing Keys with Associated Values in Hash Maps");
 
-fn ch8_3() {
-    println!("## 8.3 Storing Keys with Associated Values in Hash Maps"); // {{{
-
-    println!("### Creating a New Hash Map"); // {{{
+    println!("## Creating a New Hash Map");
     {
         use std::collections::HashMap;
         let mut scores = HashMap::new();
         scores.insert(String::from("Blue"), 10);
         scores.insert(String::from("Yellow"), 50);
     }
-    // }}}
 
-    println!("### Accessing Values in a Hash Map"); // {{{
+    println!("## Accessing Values in a Hash Map");
     {
         use std::collections::HashMap;
+
         let mut scores = HashMap::new();
+
         scores.insert(String::from("Blue"), 10);
         scores.insert(String::from("Yellow"), 50);
-        let team_name = String::from("Blue");
-        let score = scores.get(&team_name).copied().unwrap_or(0);
-        dbg!(team_name);
-        dbg!(score);
-    }
-    // }}}
 
-    // }}}
+        // Just practicing with Option<V>
+        {
+            // unwrap_or(V) provides a value if the Option<T> is None.
+            let score = scores.get("Yellow").copied().unwrap_or(0);
+            dbg!(score);
+        }
+
+        {
+            let score = match scores.get("Yellow").copied() {
+                Some(score) => score,
+                None => 0,
+            };
+            dbg!(score);
+        }
+
+        {
+            if let Some(score) = scores.get("foo").copied() {
+                println!("Got a value");
+                dbg!(score);
+            } else {
+                println!("Failed to get a value");
+            }
+        }
+    }
+
+    {}
 }
